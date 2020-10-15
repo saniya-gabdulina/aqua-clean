ba1c7edae6f2b9ecde17563ab1f9e2c326446575
saniya.gabdulina <saniya.gabdulina@gmail.com>
2020-10-15 23:07:08 +0600

    add swiper, media for 1024px

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.consist-slide').slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+    });
+});
